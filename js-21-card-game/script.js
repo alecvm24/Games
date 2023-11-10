ba6e0ac4708b220e188348949eaad7a891e49bae
Alec VM <alecvm24@hotmail.com>
2023-11-10 10:45:42 +0100

    Add functions to update player's totale

--- js-21-card-game/script.js
+++ js-21-card-game/script.js
@@ -1,13 +1,31 @@
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 
+let playerTotal = 0;
+
 function drawnCard(){
     const randomIndex = Math.floor(Math.random() * cards.length);
     const drawnCard = cards[randomIndex];
+
+
+    updateTotal(drawnCard);
+
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = 'You drew a' + drawnCard + '. Total:' + playerTotal + '.';
+
+    if (isBust()){
+        resultElement.textContent += 'BUST ! You exceeded 21.';
+        resetGame();
+    }
 }
 
-const resultElement = document.getElementById("result");
-resultElement.textContent = "You drew a" + drawnCard + ".";
-
-if (isBust(drawnCard)){
-    resultElement.textContent += "BUST ! You exceeded 21.";
+function updateTotal(cardValue){
+    if (cardValue === 1){
+        playerTotal += 11;
+        if (playerTotal > 21){
+            playerTotal -= 10;
+        }
+    } 
+    else{
+        playerTotal += cardValue;
+    }
 }
